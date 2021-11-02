40d58435cb7f883fd9305b33616baecefd162561
kuiil7 <kuiil7ig11@gmail.com>
2021-11-02 15:22:59 -0700

    removing space between strings and symbol to avoid word break

--- src/QuerySearch/MobileScrolling.js
+++ src/QuerySearch/MobileScrolling.js
@@ -27,10 +27,10 @@ is-3-desktop
 <img  src={`http://openweathermap.org/img/wn/${props.day_1_weather_icon}@2x.png`}  alt='weather icons' />
 <p className=" is-size-7 has-text-weight-bold has-text-centered ">{props.day_1_weather_description.toUpperCase() }</p>
 <ul  >
-<li className="is-size-4  " >{props.day_1_main_temp} °F</li>
+<li className="is-size-4  " >{props.day_1_main_temp}°F</li>
 <li>Humidity: {props.day_1_humidity}%</li>
-<li> Wind: {props.day_1_wind_speed} mph</li>
-<li>Gust: {props.day_1_wind_gust} mph</li>
+<li> Wind: {props.day_1_wind_speed}mph</li>
+<li>Gust: {props.day_1_wind_gust}mph</li>
 </ul>
 </div>
 </div>
@@ -47,10 +47,10 @@ is-3-desktop">
 <img  src={`http://openweathermap.org/img/wn/${props.day_2_weather_icon}@2x.png`}  alt='weather icons' />
 <p className="is-size-7 has-text-weight-bold  ">{props.day_2_weather_description.toUpperCase() }</p>
 <ul  >
-<li className="is-size-4   " >{props.day_2_main_temp} °F</li>
+<li className="is-size-4   " >{props.day_2_main_temp}°F</li>
 <li>Humidity: {props.day_2_humidity}%</li>
-<li> Wind: {props.day_2_wind_speed} mph</li>
-<li>Gust: {props.day_2_wind_gust} mph</li>
+<li> Wind: {props.day_2_wind_speed}mph</li>
+<li>Gust: {props.day_2_wind_gust}mph</li>
 </ul>
 </div>
 </div>
@@ -67,10 +67,10 @@ is-3-desktop">
 <img  src={`http://openweathermap.org/img/wn/${props.day_3_weather_icon}@2x.png`}  alt='weather icons' />
 <p className="is-size-7 has-text-weight-bold  ">{props.day_3_weather_description.toUpperCase() }</p>
 <ul  >
-<li className="is-size-4  " >{props.day_3_main_temp} °F</li>
+<li className="is-size-4  " >{props.day_3_main_temp}°F</li>
 <li>Humidity: {props.day_3_humidity}%</li>
-<li> Wind: {props.day_3_wind_speed} mph</li>
-<li>Gust: {props.day_3_wind_gust} mph</li>
+<li> Wind: {props.day_3_wind_speed}mph</li>
+<li>Gust: {props.day_3_wind_gust}mph</li>
 </ul>
 </div>
 </div>
@@ -87,10 +87,10 @@ is-3-desktop">
 <img  src={`http://openweathermap.org/img/wn/${props.day_4_weather_icon}@2x.png`}  alt='weather icons' />
 <p className="is-size-7 has-text-weight-bold  ">{props.day_4_weather_description.toUpperCase() }</p>
 <ul  >
-<li className="is-size-4  " >{props.day_4_main_temp} °F</li>
+<li className="is-size-4  " >{props.day_4_main_temp}°F</li>
 <li>Humidity: {props.day_4_humidity}%</li>
-<li> Wind: {props.day_4_wind_speed} mph</li>
-<li>Gust: {props.day_4_wind_gust} mph</li>
+<li> Wind: {props.day_4_wind_speed}mph</li>
+<li>Gust: {props.day_4_wind_gust}mph</li>
 </ul>
 </div>
 </div>
@@ -107,10 +107,10 @@ is-3-desktop">
 <img  src={`http://openweathermap.org/img/wn/${props.day_5_weather_icon}@2x.png`}  alt='weather icons' />
 <p className="is-size-7 has-text-weight-bold  ">{props.day_5_weather_description.toUpperCase() }</p>
 <ul  >
-<li className="is-size-4  " >{props.day_5_main_temp} °F</li>
+<li className="is-size-4  " >{props.day_5_main_temp}°F</li>
 <li>Humidity: {props.day_5_humidity}%</li>
-<li> Wind: {props.day_5_wind_speed} mph</li>
-<li>Gust: {props.day_5_wind_gust} mph</li>
+<li> Wind: {props.day_5_wind_speed}mph</li>
+<li>Gust: {props.day_5_wind_gust}mph</li>
 </ul>
 </div>
 </div>
